1e220770bb2b3d245e2be4b0c226a27aa8e50d3b
lossphilipp <75640684+lossphilipp@users.noreply.github.com>
2025-02-05 22:07:13 +0100

    various optimizations

--- src/main.rs
+++ src/main.rs
@@ -25,16 +25,19 @@ struct MenuText {
     font_size: u16,
 }
 
+const PADDING: f32 = 10.0;
+const FONT_SIZE_LARGE: u16 = 50;
+const FONT_SIZE_MEDIUM: u16 = 30;
+
 async fn draw_menu(texts: Vec<MenuText>) {
-    let padding = 10.0;
     let mut total_height = 0.0;
 
     // Calculate total height of all text items including padding
     for menu_text in &texts {
         let text_size = measure_text(&menu_text.text, None, menu_text.font_size, 1.0);
-        total_height += text_size.height + padding;
+        total_height += text_size.height + PADDING;
     }
-    total_height -= padding; // Remove the padding after the last item
+    total_height -= PADDING; // Remove the padding after the last item
 
     // Calculate starting y position to center all text items
     let mut current_y = screen_height() / 2.0 - total_height / 2.0;
@@ -49,7 +52,7 @@ async fn draw_menu(texts: Vec<MenuText>) {
             menu_text.font_size as f32,
             LIGHTGRAY,
         );
-        current_y += text_size.height + padding;
+        current_y += text_size.height + PADDING;
     }
 }
 
@@ -71,29 +74,40 @@ async fn _load_enemy_texture_from_binary(level: &i8, textures: &HashMap<&str, &[
     )
 }
 
-async fn load_enemy_texture_from_file(level: &i8) -> Texture2D {
-    match *level {
-        1 => load_texture("assets/python.png").await.unwrap(),
-        2 => load_texture("assets/java.png").await.unwrap(),
-        3 => load_texture("assets/dart.png").await.unwrap(),
-        4 => load_texture("assets/cplusplus.png").await.unwrap(),
-        _ => load_texture("assets/c.png").await.unwrap(),
+async fn load_texture_from_file(filename: &str) -> Texture2D {
+    let fileload_result = load_texture(&format!("assets/{}", filename)).await;
+
+    match fileload_result {
+        Ok(file) => file,
+        Err(error) => panic!("Problem opening the texture file: {error:?}"),
     }
+}
+
+async fn load_enemy_texture(level: &i8) -> Texture2D {
+    let filename = match *level {
+        1 => "python.png",
+        2 => "java.png",
+        3 => "dart.png",
+        4 => "cplusplus.png",
+        _ => "c.png",
+    };
+
+    load_texture_from_file(&filename).await
 }
 
 async fn spawn_enemies(enemies: &mut Vec<Enemy>, level: &mut i8, texture: Texture2D, rows: i8) {
     let scale = screen_width() / 20.0 / texture.width();
     let enemy_width = texture.width() * scale;
     let enemy_height = texture.height() * scale;
-    let spacing_x = enemy_width + 10.0;
-    let spacing_y = enemy_height + 10.0;
+    let spacing_x = enemy_width + PADDING;
+    let spacing_y = enemy_height + PADDING;
 
     let texture_pointer = Rc::new(texture);
 
     for row in 0..rows {
         for col in 0..10 {
-            let x = col as f32 * spacing_x + 11.0;
-            let y = row as f32 * spacing_y + 10.0;
+            let x = col as f32 * spacing_x + PADDING + 1.0;
+            let y = row as f32 * spacing_y + PADDING;
 
             enemies.push(Enemy::new(
                 Rc::clone(&texture_pointer),
@@ -120,8 +134,7 @@ async fn init_game (
 
     enemies.clear();
 
-    // let current_enemy_texture = load_enemy_texture(level, enemy_textures).await;
-    let current_enemy_texture = load_enemy_texture_from_file(level).await;
+    let current_enemy_texture = load_enemy_texture(level).await;
     spawn_enemies(enemies, level, current_enemy_texture, 5).await;
 
     *game_state = GameState::Playing;
@@ -131,14 +144,14 @@ async fn calculate_enemy_movement(enemies: &mut Vec<Enemy>, delta_time: &f32) {
     let screen_width = screen_width();
     let mut hit_wall = false;
 
-    let first_enemy = enemies.first().unwrap();
+    let first_enemy = enemies.first().unwrap(); //We are sure enemies are always present → unwrap is safe
     let enemy_width = first_enemy.texture.width() * first_enemy.scale;
 
     // ToDo: Sometimes this is bugged and moves all enemies directly to the bottom
     //       I assume the enemies get updated to often and glitch into the padding,
     //       which triggers the direction change multiple times
     for enemy in enemies.iter() {
-        if enemy.position.x + enemy_width >= screen_width - 10.0 || enemy.position.x <= 10.0 {
+        if enemy.position.x + enemy_width >= screen_width - PADDING || enemy.position.x <= PADDING {
             hit_wall = true;
             break;
         }
@@ -150,7 +163,7 @@ async fn calculate_enemy_movement(enemies: &mut Vec<Enemy>, delta_time: &f32) {
     }
 }
 
-// Possible optimization: use bullet pool instead of creating & deleting new bullets every time
+// ToDo: Possible optimization: use bullet pool instead of creating & deleting new bullets every time
 async fn shoot_bullet(bullets: &mut Vec<Bullet>, player: &mut Player, last_shot: &mut f64, delta_time: &f32, score: &mut i32) {
     let bullet_shoot_speed = 50.0;
     let current_time = get_time();
@@ -198,130 +211,148 @@ async fn check_round_finished(player: &Player, enemies: &Vec<Enemy>, game_state:
     }
 }
 
+async fn handle_menu(game_state: &mut GameState, level: &mut i8, player: &mut Player, enemies: &mut Vec<Enemy>, bullets: &mut Vec<Bullet>) {
+    draw_menu(vec![
+        MenuText { text: "CodeInvaders".to_string(), font_size: FONT_SIZE_LARGE },
+        MenuText { text: "Press ENTER to Start".to_string(), font_size: FONT_SIZE_MEDIUM },
+        MenuText { text: "Press H to show highscores".to_string(), font_size: FONT_SIZE_MEDIUM },
+    ]).await;
+
+    if is_key_pressed(KeyCode::Enter) {
+        init_game(level, player, enemies, bullets, game_state).await;
+    }
+    if is_key_pressed(KeyCode::H) {
+        *game_state = GameState::HighScores;
+    }
+}
+
+async fn handle_playing(game_state: &mut GameState, player: &mut Player, enemies: &mut Vec<Enemy>, bullets: &mut Vec<Bullet>, last_shot: &mut f64, delta_time: &f32, score: &mut i32) {
+    player.update(&delta_time);
+    player.draw();
+
+    calculate_enemy_movement(enemies, &delta_time).await;
+
+    shoot_bullet(bullets, player, last_shot, &delta_time, score).await;
+
+    for bullet in bullets.iter_mut() {
+        bullet.update(&delta_time);
+        bullet.draw();
+    }
+    bullets.retain(|bullet| bullet.position.y < screen_height());
+
+    check_collision(bullets, enemies, score).await;
+
+    check_round_finished(&player, &enemies, game_state).await;
+
+    if is_key_pressed(KeyCode::Escape) {
+        *game_state = GameState::EnterName;
+    }
+}
+
+async fn handle_level_complete(game_state: &mut GameState, level: &mut i8, player: &mut Player, enemies: &mut Vec<Enemy>, bullets: &mut Vec<Bullet>, score: &i32) {
+    draw_menu(vec![
+        MenuText { text: "LEVEL COMPLETE".to_string(), font_size: FONT_SIZE_LARGE },
+        MenuText { text: format!("SCORE: {score}"), font_size: FONT_SIZE_LARGE },
+        MenuText { text: "Press ENTER to continue".to_string(), font_size: FONT_SIZE_MEDIUM },
+        MenuText { text: "Press ESC to finish".to_string(), font_size: FONT_SIZE_MEDIUM },
+    ]).await;
+
+    if is_key_pressed(KeyCode::Enter) {
+        init_game(level, player, enemies, bullets, game_state).await;
+        return;
+    }
+    if is_key_pressed(KeyCode::Escape) {
+        *level = 0;
+        *game_state = GameState::EnterName;
+    }
+}
+
+async fn handle_game_over(game_state: &mut GameState, score: &mut i32, level: &mut i8) {
+    draw_menu(vec![
+        MenuText { text: "GAME OVER".to_string(), font_size: FONT_SIZE_LARGE },
+        MenuText { text: format!("SCORE: {score}"), font_size: FONT_SIZE_LARGE },
+        MenuText { text: "Press ESC to finish".to_string(), font_size: FONT_SIZE_MEDIUM },
+        MenuText { text: "Press H to show highscores".to_string(), font_size: FONT_SIZE_MEDIUM },
+    ]).await;
+
+    if is_key_pressed(KeyCode::Escape) ||
+       is_key_pressed(KeyCode::Enter) {
+        *level = 0;
+        *score = 0;
+        *game_state = GameState::Menu;
+        return;
+    }
+    if is_key_pressed(KeyCode::H) {
+        *level = 0;
+        *score = 0;
+        *game_state = GameState::HighScores;
+    }
+}
+
+async fn handle_enter_name(game_state: &mut GameState, high_scores: &mut HighScores, name_input: &mut NameInput, score: &i32) {
+    if !high_scores.qualifies(*score) {
+        *game_state = GameState::GameOver;
+        return;
+    }
+
+    name_input.update();
+    name_input.draw();
+
+    if is_key_pressed(KeyCode::Enter) {
+        high_scores.add_score(name_input.name.clone(), *score);
+        *game_state = GameState::GameOver;
+    }
+}
+
+async fn handle_high_scores(game_state: &mut GameState, high_scores: &HighScores) {
+    let mut scores = high_scores.display().iter().map(
+        |entry| MenuText { text: entry.clone(), font_size: FONT_SIZE_MEDIUM }
+    ).collect::<Vec<MenuText>>();
+
+    let mut menu_texts = vec![
+        MenuText { text: "HIGH SCORES".to_string(), font_size: FONT_SIZE_LARGE },
+        MenuText { text: "Press ESC to exit".to_string(), font_size: FONT_SIZE_MEDIUM },
+        MenuText { text: " ".to_string(), font_size: FONT_SIZE_MEDIUM },
+    ];
+
+    menu_texts.append(&mut scores);
+
+    draw_menu(menu_texts).await;
+
+    if is_key_pressed(KeyCode::Escape) {
+        *game_state = GameState::Menu;
+    }
+}
+
 #[macroquad::main("CodeInvaders")]
 async fn main() {
+    // Initiate globaly needed game assets
     let mut delta_time;
     let mut game_state = GameState::Menu;
     let mut level: i8 = 0;
     let mut score: i32 = 0;
     let mut high_scores = HighScores::new();
-    high_scores.load().unwrap_or_default();
+    high_scores.load().unwrap_or_default(); // right now i implemented this to always return true, so no use of error handling
     let mut name_input = NameInput::new();
-    let mut player = Player::new(load_texture("assets/rust.png").await.unwrap());
+    let mut player = Player::new(load_texture_from_file("rust.png").await);
     let mut enemies: Vec<Enemy> = Vec::new();
     let mut bullets: Vec<Bullet> = Vec::new();
     let mut last_shot = get_time();
 
+    // Run game
+    clear_background(BLACK);
     loop {
         delta_time = get_frame_time();
-        clear_background(BLACK);
 
         match game_state {
-            GameState::Menu => {
-                draw_menu(vec![
-                    MenuText { text: "CodeInvaders".to_string(), font_size: 50 },
-                    MenuText { text: "Press ENTER to Start".to_string(), font_size: 30 },
-                    MenuText { text: "Press H to show highscores".to_string(), font_size: 30 },
-                ]).await;
-
-                if is_key_pressed(KeyCode::Enter) {
-                    init_game(&mut level, &mut player, &mut enemies, &mut bullets, &mut game_state).await;
-                }
-                if is_key_pressed(KeyCode::H) {
-                    game_state = GameState::HighScores;
-                }
+            // ToDo: This looks pretty bad. Find a better way to handle globaly needed stuff like the enemy list
+            GameState::Menu => handle_menu(&mut game_state, &mut level, &mut player, &mut enemies, &mut bullets).await,
+            GameState::Playing => handle_playing(&mut game_state, &mut player, &mut enemies, &mut bullets, &mut last_shot, &delta_time, &mut score).await,
+            GameState::LevelComplete => handle_level_complete(&mut game_state, &mut level, &mut player, &mut enemies, &mut bullets, &score).await,
+            GameState::GameOver => handle_game_over(&mut game_state, &mut score, &mut level).await,
+            GameState::EnterName => handle_enter_name(&mut game_state, &mut high_scores, &mut name_input, &score).await,
+            GameState::HighScores => handle_high_scores(&mut game_state, &high_scores).await,
             }
-            GameState::Playing => {
-                player.update(&delta_time);
-                player.draw();
-
-                calculate_enemy_movement(&mut enemies, &delta_time).await;
-
-                shoot_bullet(&mut bullets, &mut player, &mut last_shot, &delta_time, &mut score).await;
-
-                for bullet in bullets.iter_mut() {
-                    bullet.update(&delta_time);
-                    bullet.draw();
-                }
-                bullets.retain(|bullet| bullet.position.y < screen_height());
-
-                check_collision(&mut bullets, &mut enemies, &mut score).await;
-
-                check_round_finished(&player, &enemies, &mut game_state).await;
-
-                if is_key_pressed(KeyCode::Escape) {
-                    game_state = GameState::EnterName;
-                }
-            }
-            GameState::LevelComplete => {
-                draw_menu(vec![
-                    MenuText { text: "LEVEL COMPLETE".to_string(), font_size: 50 },
-                    MenuText { text: format!("SCORE: {score}"), font_size: 50 },
-                    MenuText { text: "Press ENTER to continue".to_string(), font_size: 30 },
-                    MenuText { text: "Press ESC to finish".to_string(), font_size: 30 },
-                ]).await;
-
-                if is_key_pressed(KeyCode::Enter) {
-                    init_game(&mut level, &mut player, &mut enemies, &mut bullets, &mut game_state).await;
-                }
-                if is_key_pressed(KeyCode::Escape) {
-                    level = 0;
-                    game_state = GameState::EnterName;
-                }
-            }
-            GameState::GameOver => {
-                draw_menu(vec![
-                    MenuText { text: "GAME OVER".to_string(), font_size: 50 },
-                    MenuText { text: format!("SCORE: {score}"), font_size: 50 },
-                    MenuText { text: "Press ESC to finish".to_string(), font_size: 30 },
-                    MenuText { text: "Press H to show highscores".to_string(), font_size: 30 },
-                ]).await;
-
-                if is_key_pressed(KeyCode::Escape) ||
-                   is_key_pressed(KeyCode::Enter) {
-                    level = 0;
-                    score = 0;
-                    game_state = GameState::Menu;
-                }
-                if is_key_pressed(KeyCode::H) {
-                    level = 0;
-                    score = 0;
-                    game_state = GameState::HighScores;
-                }
-            }
-            GameState::EnterName => {
-                if !high_scores.qualifies(score) {
-                    game_state = GameState::GameOver;
-                }
-
-                name_input.update();
-                name_input.draw();
-
-                if is_key_pressed(KeyCode::Enter) {
-                    high_scores.add_score(name_input.name.clone(), score);
-                    game_state = GameState::GameOver;
-                }
-            }
-            GameState::HighScores => {
-                let mut scores = high_scores.display().iter().map(
-                    |entry| MenuText { text: entry.clone(), font_size: 30 }
-                ).collect::<Vec<MenuText>>();
-
-                let mut menu_texts = vec![
-                    MenuText { text: "HIGH SCORES".to_string(), font_size: 50 },
-                    MenuText { text: "Press ESC to exit".to_string(), font_size: 30 },
-                    MenuText { text: " ".to_string(), font_size: 30 },
-                ];
-
-                menu_texts.append(&mut scores);
-
-                draw_menu(menu_texts).await;
-
-                if is_key_pressed(KeyCode::Escape) {
-                    game_state = GameState::Menu;
-                }
-            }
-        }
 
         next_frame().await
     }
